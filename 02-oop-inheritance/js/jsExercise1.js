0e67ba714d0b30f3ff65f4062f6e29493d8832fd
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-13 15:53:31 -0300

    topic 2 partial

---
+++ 02-oop-inheritance/js/jsExercise1.js
@@ -0,0 +1,31 @@
+var Movie = Object.create(null);
+Movie.play = function(){
+
+}
+Movie.pause = function(){
+
+}
+Movie.resume = function(){
+
+}
+
+var myMovie = Object.create(Movie)
+myMovie.title = "Spider Man";
+console.log(myMovie.title)
+
+var movie = {
+  title,
+  year,
+  duration,
+  play: function(){
+
+  },
+  pause: function(){
+
+  },
+  resume: function(){
+
+  }
+}
+
+//var myMovie = new movie("Spider Man", "2016", )
