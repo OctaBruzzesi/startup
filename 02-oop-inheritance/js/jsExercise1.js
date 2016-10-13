e51789c656fe6e10333cf9104bfef0a92800ecd3
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-13 17:04:02 -0300

    Octavio Bruzzesi, Topic 2: Exercise 3 started.

--- 02-oop-inheritance/js/jsExercise1.js
+++ 02-oop-inheritance/js/jsExercise1.js
@@ -1,31 +1,33 @@
-var Movie = Object.create(null);
-Movie.play = function(){
+class classMovie{
+  constructor(title, year, duration){
+    this.title = title;
+    this.year = year;
+    this.duration = duration;
+  }
+  play(){
+      console.log("aaa");
+  }
+  pause(){
 
-}
-Movie.pause = function(){
-
-}
-Movie.resume = function(){
-
-}
-
-var myMovie = Object.create(Movie)
-myMovie.title = "Spider Man";
-console.log(myMovie.title)
-
-var movie = {
-  title,
-  year,
-  duration,
-  play: function(){
-
-  },
-  pause: function(){
-
-  },
-  resume: function(){
+  }
+  resume(){
 
   }
 }
 
-//var myMovie = new movie("Spider Man", "2016", )
+
+let Movie = new classMovie("Spider Man", "2016", "120");
+console.log(Movie);
+Movie.play();
+
+class EventEmitter{
+  on(){
+
+  }
+  emit(){
+
+  }
+  off(){
+
+  }
+}
