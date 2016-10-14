520c966f5a9c0ba4aa5ab972f52089b9b05818ec
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-14 17:22:14 -0300

    Octavio Bruzzesi, Topic 2: Exercise 3 started.

--- 02-oop-inheritance/js/jsExercise1.js
+++ 02-oop-inheritance/js/jsExercise1.js
@@ -5,7 +5,7 @@ class classMovie{
     this.duration = duration;
   }
   play(){
-      console.log("aaa");
+
   }
   pause(){
 
@@ -15,19 +15,52 @@ class classMovie{
   }
 }
 
-
 let Movie = new classMovie("Spider Man", "2016", "120");
 console.log(Movie);
 Movie.play();
 
-class EventEmitter{
-  on(){
 
+
+class EventEmitter {
+  constructor() {
+    this.listeners = new Map();
   }
-  emit(){
-
+  on(event, callback) {
+    this.listeners.has(event) || this.listeners.set(event, []);
+    this.listeners.get(event).push(callback);
   }
-  off(){
+  removeListener(event, callback) {
+    let listeners = this.listeners.get(event),
+        index;
 
+    if (listeners && listeners.length) {
+        index = listeners.reduce((i, listener, index) => {
+            return (isFunction(listener) && listener === callback) ?
+                i = index :
+                i;
+        }, -1);
+
+        if (index > -1) {
+            listeners.splice(index, 1);
+            this.listeners.set(event, listeners);
+            return true;
+        }
+    }
+    return false;
+  }
+  emit(event, ...args) {
+    let listeners = this.listeners.get(event);
+
+    if (listeners && listeners.length) {
+        listeners.forEach((listener) => {
+            listener(...args);
+        });
+        return true;
+    }
+    return false;
   }
 }
+
+var myEmitter = new EventEmitter;
+myEmitter.on(Movie.play(), function (Movie) {console.log("Emitted " + Movie.title)});
+myEmitter.emit(Movie.play(), Movie);
