0eb8fb34c79de81a1c4389b42a6b50a1632889be
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-17 17:59:08 -0300

    Octavio Bruzzesi, topic 2: Exercise 6 done.

--- 02-oop-inheritance/js/jsExercise1.js
+++ 02-oop-inheritance/js/jsExercise1.js
@@ -1,20 +1,24 @@
 class EventEmitter {
- constructor () {
-   this.listeners = [];
- }
+  constructor () {
+    this.listeners = {};
+  }
 
- on (event, callback) {
-   if(this.listeners[event] != callback){
-     this.listeners[event] = callback
-   }
- }
+  on (event, callback) {
+    if(this.listeners[event] !== callback){
+      this.listeners[event] = callback
+    }
+  }
 
- off (event, callback) {
-   if(this.listeners[event]) this.listeners[event] = undefined;
- }
+  off (event, callback) {
+    if(this.listeners[event]) {
+      delete this.listeners[event];
+    }
+  }
 
- emit (movie, event) {
-    if(this.listeners[event]) this.listeners[event](movie, event);
+  emit (movie, event) {
+    if(this.listeners[event]) {
+      this.listeners[event](movie, event)
+    }
   }
 }
 
@@ -28,31 +32,38 @@ class logger {
   }
 }
 
-let myEmitter = new EventEmitter;
-let mylogger = new logger;
-
 class classMovie extends EventEmitter{
- constructor(title, year, duration){
-   super();
-   this.title = title;
-   this.year = year;
-   this.duration = duration;
- }
- play(){
-   super.emit(this, 'play');
- }
- pause(){
-   super.emit(this, 'pause');
- }
- resume(){
-   super.emit(this, 'resume');
- }
+  constructor(title, year, duration){
+    super();
+    this.title = title;
+    this.year = year;
+    this.duration = duration;
+  }
+  play(){
+    super.emit(this, 'play');
+  }
+  pause(){
+    super.emit(this, 'pause');
+  }
+  resume(){
+    super.emit(this, 'resume');
+  }
 }
+
+let Social = {
+
+  share: function (friendName) {
+    console.log("Share " + this.title + " with " + friendName);
+  },
+  like: function (friendName) {
+    console.log(friendName + " liked " + this.title);
+  }
+}
+
+let mylogger = new logger;
 
 let Movie1 = new classMovie("Spider Man", "2016", "120");
 let Movie2 = new classMovie("Iron Man", "2015", "95");
-console.log(Movie1);
-
 
 Movie1.on('play', mylogger.log);
 Movie1.play();
@@ -63,6 +74,12 @@ Movie1.resume();
 Movie1.off('play', mylogger.log);
 Movie1.play();
 
+Object.assign(Movie1, Social);
+
+console.log(Movie1);
+
+Movie1.share("John Frusciante");
+Movie1.like("John Frusciante");
 //
 // Movie1.pause();
 //
