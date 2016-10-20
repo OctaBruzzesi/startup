86cd28e5ae4031a1e818a9ecbbc40d1138171670
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-20 17:53:22 -0300

    Octavio Bruzzesi, Topic 4: Exercise 2 done

--- 04-HTML5-APIs/js/jsExercise1.js
+++ 04-HTML5-APIs/js/jsExercise1.js
@@ -1,41 +1,56 @@
-function storeTextArea () {
+function storeLocalTextArea () {
   let input = {key: 0, text: document.getElementById("inputTextArea").value};
   localStorage.setItem("inputTextStored", input.text);
   document.getElementById("a").innerHTML = localStorage.getItem("inputTextStored");
+}
 
-  window.indexedDB = window.indexedDB
-  let db;
-  let request = indexedDB.open("MyDatabase", 3);
-  request.onerror = function (event) {
-    alert("Why didn't you allow my web app to use IndexedDB?!");
+function createIDBTextArea () {
+  let IDB = window.indexedDB;
+  db = IDB.open("textIndexDataBase", 1);
+
+  db.onupgradeneeded = function (event) {
+    let dataBase = event.target.result;
+    textObject = dataBase.createObjectStore("textDataBase", { keyPath : 'text' });
+    textObject.createIndex('Text', 'text', { unique : false });
   }
+
+  db.onerror = function (event) {
+    alert("Indexed Data Base not allowed, please enable it");
+  }
+
+  db.onsuccess = function (event){
+    //alert("Indexed Data Base create successfully");
+  }
+}
+
+function saveIDBTextArea () {
+  let result = db.result;
+  let textDataBaseTransaction = result.transaction("textDataBase", "readwrite").objectStore("textDataBase");
+  let textArea = document.getElementById("inputTextArea").value;
+  let request = textDataBaseTransaction.put( {
+    keyPath: 1,
+    text: textArea
+  });
 
   request.onsuccess = function (event) {
-    console.log("a");
-    db = event.target.result;
-    alert(db.get(0));
+    alert("Text added");
+    textArea = "";
   }
 
-  request.onupgradeneeded = function (event) {
-    console.log("e");
-    var db = event.target.result;
-    // Create an objectStore for this database
-     var objectStore = db.createObjectStore("texts", { keyPath: "key" });
-     objectStore.createIndex("name", "name", { unique: false });
-     objectStore.transaction.oncomplete = function(event) {
-    // Store values in the newly created objectStore.
-      var customerObjectStore = db.transaction("texts", "readwrite").objectStore("texts");
-      customerObjectStore.add(input)
-    }
+  request.onerror = function(event) {
+    alert("Something went wrong");
   }
-  var transaction = db.transaction(["texts"]);
-  var objectStore = transaction.objectStore("texts");
-  var request3 = objectStore.get("0");
-  request3.onerror = function(event) {
-    // Handle errors!
-  };
-  request3.onsuccess = function(event) {
-    // Do something with the request.result!
-    alert("Input Text:  " + request.result.name);
-  };
+}
+
+function deleteLocalTextArea () {
+  localStorage.clear();
+}
+
+function deleteIDBTextArea () {
+  let result = db.result;
+  var request = result.transaction("textDataBase", "readwrite").objectStore("textDataBase").clear();
+
+  request.onsuccess = function (event) {
+    alert("Remove");
+  }
 }
