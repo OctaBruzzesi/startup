fc695e372b36112b95e77e6ac783e5649ad70601
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-24 14:55:03 -0300

    Octavio Bruzzesi, Topic 4: Exercise 7 done.

---
+++ 04-HTML5-APIs/js/canvasAnimated.js
@@ -0,0 +1,32 @@
+function initialize () {
+  let x =  0;
+  let y = 5;
+  let drawSpeedx = 3;
+  let drawSpeedy = 4;
+
+  function animateDraw() {
+    let animationFrame = window.mRequestAnimationFrame||window.webkitRequestAnimationFrame||window.msRequestAnimationFrame||
+    window.oRequestAnimationFrame; // supports different browsers
+    animationFrame(animateDraw);
+    x += drawSpeedx;
+    y += drawSpeedy;
+    if(x <= 0 || x >= 350){
+      drawSpeedx = -drawSpeedx;
+    }
+    if(y <= 0 || y >= 350){
+      drawSpeedy = -drawSpeedy;
+    }
+
+      draw();
+  }
+
+  function draw() {
+      let canvas  = document.getElementById("canvas");
+      let context = canvas.getContext("2d");
+      context.clearRect(0, 0, 500, 500);
+      context.fillStyle = "#R89EE9";
+      context.fillRect(x, y, 100, 30);
+  }
+
+  animateDraw();
+}
--- 04-HTML5-APIs/js/jsExercise1.js
+++ 04-HTML5-APIs/js/jsExercise1.js
@@ -54,3 +54,100 @@ function deleteIDBTextArea () {
     alert("Remove");
   }
 }
+
+//Exercise 3 drop zone
+
+function allowDrop(ev){
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "copy";
+}
+
+var holder = document.getElementById('drop'),
+    state = document.getElementById('status');
+
+if (typeof window.FileReader === 'undefined') {
+    state.className = 'fail';
+} else {
+    state.className = 'success';
+    state.innerHTML = 'File API & FileReader available';
+}
+
+holder.ondragover = function() {
+    this.className = 'hover';
+    return false;
+};
+holder.ondragend = function() {
+    this.className = '';
+    return false;
+};
+holder.ondrop = function(e) {
+    this.className = '';
+    e.preventDefault();
+
+    var file = e.dataTransfer.files,
+        reader = new FileReader();
+    reader.onload = function(event) {
+      document.getElementById('drop').value = event.target.result;
+        console.log(event.target);
+    };
+    reader.readAsText(file[0],"UTF-8");
+}
+
+// exercise 4 socket
+
+
+  var wsUri = "ws://echo.websocket.org/";
+  var output;
+
+  function init()
+  {
+    output = document.getElementById("output");
+    testWebSocket();
+  }
+
+  function testWebSocket()
+  {
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+  }
+
+  function onOpen(evt)
+  {
+    writeToScreen("CONNECTED");
+    doSend("WebSocket rocks");
+  }
+
+  function onClose(evt)
+  {
+    writeToScreen("DISCONNECTED");
+  }
+
+  function onMessage(evt)
+  {
+    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+    websocket.close();
+  }
+
+  function onError(evt)
+  {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+  }
+
+  function doSend(message)
+  {
+    writeToScreen("SENT: " + message);
+    websocket.send(message);
+  }
+
+  function writeToScreen(message)
+  {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
+  }
+
+  window.addEventListener("load", init, false);
